7eef96528f2f01f35455106b3025808bcd236a9b
Jekin Gohel <jekin1991@gmail.com>
2024-04-03 15:28:34 +0530

    Show seat selection

--- src/components/Reservation/TripDetails/BusSeatDetails.tsx
+++ src/components/Reservation/TripDetails/BusSeatDetails.tsx
@@ -1,6 +1,10 @@
 import { CardContent } from "components/ui/Card"
+import { useSelector } from "react-redux"
+import { Passenger } from "store/types"
 
 const BusDetails = () => {
+  const { passangers } = useSelector((state: any) => state.CurrentReservation)
+  const seats = passangers.map((p: Passenger) => p.seat)
   return (
     <CardContent className="w-full px-4 pb-0">
       <div className="w-full inline-flex flex-row justify-between items-center">
@@ -14,7 +18,9 @@ const BusDetails = () => {
           <label className="font-normal text-sm text-muted-foreground">
             Seat No
           </label>
-          <h3 className="text-md font-semibold">DU4,DU3</h3>
+          <h3 className="text-md font-semibold">
+            {seats.length > 0 ? seats.join(", ") : "-"}
+          </h3>
         </div>
       </div>
       <div className="border-dotted border-b-2 border-slate-200 my-2" />
